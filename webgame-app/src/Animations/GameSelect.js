23b174080af8843e2801572ee00dc17a6bcd7a5c
wilsonhsu0102 <wilsonhsu0102@gmail.com>
2019-07-04 09:14:17 -0400

    Created GameSelectAnimation and AppManager

--- webgame-app/src/Animations/GameSelect.js
+++ webgame-app/src/Animations/GameSelect.js
@@ -2,23 +2,19 @@ import React from 'react';
 
 const fps = 200; //milliseconds
 
-class Animation extends React.Component {
+class GameSelect extends React.Component {
     constructor(props) {
       super(props);
-      this.state = this.props.state;
       this.updateAnimationState = this.updateAnimationState.bind(this);
-    //   this.keyPress = this.keyPress.bind(this);
       this.delayFunction = this.delayFunction.bind(this);
     }
     
     componentDidMount() {
-    //   document.addEventListener("keydown", this.keyPress, false);
       this.rAF = requestAnimationFrame(this.updateAnimationState);
       setInterval(this.delayFunction, fps);
     }
     
     componentWillUnmount() {
-    //   document.removeEventListener("keydown", this.keyPress, false);
       cancelAnimationFrame(this.rAF);
     }
 
@@ -26,25 +22,12 @@ class Animation extends React.Component {
         this.rAF = requestAnimationFrame(this.updateAnimationState);
     }
 
-    updateState(state) {
-
-    }
-
     updateAnimationState() {
-        // this.setState(prevState => ({ game: prevState.game, dx: prevState.dx, dy: prevState.dy, test: prevState.test }));
-        this.setState(prevState => ({ game: prevState.game, dx: prevState.dx, dy: prevState.dy, test: prevState.test }));
-
     }
     
     render() {
-        if(this.state.game === "Snake") {
-            console.log("1:" + this.state);
-            return <Canvas dx={this.state.dx} dy={this.state.dy} mode={1}/>;
-        }
-        else {
-            console.log("2:" + this.state);
-            return <Canvas dx={this.state.dx} dy={this.state.dy} mode={2}/>;
-        }
+      console.log("props:" + this.props.mode);
+      return <Canvas mode={this.props.mode}/>;
     }
   }
 
@@ -52,26 +35,29 @@ class Canvas extends React.Component {
     constructor(props) {
       super(props);
       this.canvasRef = React.createRef();
-      this.pos = {dx: this.props.dx, dy: this.props.dy};
-      this.mode = this.props.mode;
     }
     
     componentDidUpdate() {
       const canvas = this.canvasRef.current;
       const ctx = canvas.getContext('2d');
+      console.log(this.mode);
       
-      if(this.mode === 1) {
+      if(this.props.mode === 1) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-      } else if(this.mode === 2){
+      } else if(this.props.mode === 2){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = "red";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "blue";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
       }
     
       ctx.fillStyle = "white";
-      ctx.fillRect(this.pos.dx, this.pos.dy, 10, 10);
+      ctx.fillRect(1, 1, 10, 10);
     }
       
     render() {
@@ -79,4 +65,4 @@ class Canvas extends React.Component {
     }
   }
 
-  export default Animation;
+  export default GameSelect;
--- webgame-app/src/Game/AppManager.js
+++ webgame-app/src/Game/AppManager.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../css/App.css';
-import Animation from '../Animations/gameLoadScreen';
-
-const fps = 200; //ms
+import GameSelect from '../Animations/GameSelect';
 
 class AppManager extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      screen: "GameSelect",
+      select: "GameSelect"
+    }
     this.keyPress = this.keyPress.bind(this);
   }
 
@@ -21,23 +23,43 @@ class AppManager extends React.Component {
   keyPress(event) {
     switch (event.keyCode) {
       case 37:
-          break;
+        this.setState({select: "Left"});
+        break;
       case 38:
-          break;
-      case 39: 
-          break;
-      case 40: 
-          break;
-      case 13: 
-          break;
+        this.setState({select: "Up"});
+        break;
+      case 39:
+        this.setState({select: "Right"});
+        break;
+      case 40:
+        this.setState({select: "Down"});
+        break;
+      case 13:
+        if(this.state.screen !== this.state.select) {
+          this.setState({screen: this.state.select});
+        }
+
+        break;
       default:
     } 
   }
 
   render() {
+    if (this.state.screen === "GameSelect") {
+      this.changeScreen = <GameSelect mode={0}/>;
+    } else if (this.state.screen === "Left" || this.state.screen === "Right") {
+      this.changeScreen = <GameSelect mode={1}/>;
+    } else if (this.state.screen === "Up" || this.state.screen === "Down") {
+      this.changeScreen = <GameSelect mode={2}/>;
+    } else {
+      this.changeScreen = "SOMETHING WENT WRONG";
+    }
     return (
       <div>
         <header className="App-header">
+          <div> {this.changeScreen} </div>
+          <div> screen: {this.state.screen} </div>
+          <div> select: {this.state.select} </div>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
