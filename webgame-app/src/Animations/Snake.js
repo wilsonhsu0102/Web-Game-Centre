5fb05cfa492a258ebcb52b35c7ea1e73ea790f5d
wilsonhsu0102 <wilsonhsu0102@gmail.com>
2019-07-09 22:18:02 -0400

    added game over screen

--- webgame-app/src/Animations/Snake.js
+++ webgame-app/src/Animations/Snake.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-export default class SnakeCanvas extends React.Component {
+class SnakeCanvas extends React.Component {
     constructor(props) {
       super(props);
       this.canvasRef = React.createRef();
       this.snake = this.props.snake;
-      this.snacc = {
-          posX: this.props.snaccPosX,
-          posY: this.props.snaccPosY
-      }
+      this.snacc = this.props.snacc;
     }
     
     componentDidUpdate() {
@@ -17,30 +14,53 @@ export default class SnakeCanvas extends React.Component {
       const width = 10;
       const height = 10;
 
-      if (this.props.gameOver === true) {
-        ctx.font = "80px serif";
-        ctx.fillText("GAME OVER", canvas.width/3.7, canvas.height/2, 300000);
-      } 
-      else {
-        
-        
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-        var currSnake = this.snake.head;
-        while (currSnake != null) {
-          ctx.fillStyle = "white";
-          ctx.fillRect(currSnake.posX * width + 1, currSnake.posY * height + 1, 9, 9);
-          currSnake = currSnake.next;
-        }
-  
-        ctx.fillStyle = "yellow";
-        ctx.fillRect(this.snacc.posX * width + 1, this.snacc.posY * height + 1, 9, 9);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      var currSnake = this.snake.head;
+      console.log(currSnake.posX, currSnake.posY);
+      while (currSnake != null) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(currSnake.posX * width + 1, currSnake.posY * height + 1, 9, 9);
+        currSnake = currSnake.next;
       }
+
+      ctx.fillStyle = "yellow";
+      ctx.fillRect(this.snacc.randomPosX * width + 1, this.snacc.randomPosY * height + 1, 9, 9);
+    
     }
       
     render() {
       return <canvas width="900" height="600" ref={this.canvasRef}></canvas>;
     }
   }
+
+class GameOverCanvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    console.log("got here");
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "yellow";
+    ctx.font = "80px serif";
+    ctx.fillText("GAME OVER", 225, 250);
+    ctx.font = "26px sans-serif";
+    ctx.fillText("Score: " + this.props.score, 400, 375);
+  }
+
+  render() {
+    return <canvas width="900" height="600" ref={this.canvasRef}></canvas>
+  }
+}
+
+export {SnakeCanvas, GameOverCanvas};
